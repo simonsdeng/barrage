3be8ae7629ff1370053f4d4646d823c680f2b7c3
Vikram Idury <vikramidury@gmail.com>
2014-05-15 09:50:09 -0700

    Add getters and setters for height, width, x, and y for the Projectile

--- src/Projectile.java
+++ src/Projectile.java
@@ -60,6 +60,37 @@ public abstract class Projectile {
 		this.theta = theta;
 	}
 	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
 
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
 	
 }
