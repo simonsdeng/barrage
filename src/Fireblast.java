f9bc8014fffb57656f26ad9419d5883259e5d68e
Nikhil Ghosh <nikghosh09@gmail.com>
2014-05-15 09:57:08 -0700

    Create basic spell

---
+++ src/Fireblast.java
@@ -0,0 +1,23 @@
+import java.awt.Image;
+
+
+
+public class Fireblast extends Projectile {
+	private int level;
+	private int r;
+	
+	public Fireblast(int x, int y, int height, int width, int level, double direction, Image image) {
+		super(x, y, height, width, direction, image);
+		this.level = level;
+		r = width/2;
+	}
+	
+	public boolean isOnScreen() {
+		return getX() >= r && getX() <= Barrage.WIDTH - r && getY() >= r && getY() <= Barrage.HEIGHT - r;
+	}
+	
+	
+	
+	
+
+}
