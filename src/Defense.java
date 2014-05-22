e81508fb364734764554ccef8f5946ce7e80c0cf
Vikram Idury <vikramidury@gmail.com>
2014-05-21 22:52:40 -0700

    Modify Structure and Defense framework to accomodate Enemies

--- src/Defense.java
+++ src/Defense.java
@@ -1,12 +1,28 @@
 import java.awt.Image;
+import java.awt.Point;
+import java.util.ArrayList;
 
 public abstract class Defense extends Structure {
 
 	protected int health;
 	protected int cost;
+	protected int range;
+	
 
 	public Defense(int x, int y, Image icon) {
 		super(x, y, icon);
+	}
+	
+	public void setRange(int range){
+		this.range = range;
+	}
+	
+	public int getRange(){
+		return range;
+	}
+	
+	public void setCost(int cost){
+		this.cost = cost;
 	}
 	
 	public int getCost() {
@@ -19,6 +35,20 @@ public abstract class Defense extends Structure {
 	
 	public int getHealth() {
 		return health;
+	}
+
+	
+	public ArrayList<Enemy> getEnemiesInProximity(ArrayList<Enemy> enemies){
+		ArrayList<Enemy> close = new ArrayList<Enemy>();
+		for (Enemy enemy: enemies){
+			Point e = new Point(enemy.getX(), enemy.getY());
+			Point p = new Point(getX(), getY());
+			double dist = e.distance(p);
+			if (dist - range < .0001){
+				close.add(enemy);
+			}
+		}
+		return close;
 	}
 
 }
--- src/Structure.java
+++ src/Structure.java
@@ -3,32 +3,43 @@ import java.awt.Image;
 /**
  * @author Vikram Idury
  * 
- * Represents a basic structure on the map. The structures are bound by an imaginary grid of squares.
+ *         Represents a basic structure on the map. The structures are bound by
+ *         an imaginary grid of squares.
  */
 public abstract class Structure extends Entity {
 
 	protected int gridX;
 	protected int gridY;
 	protected Image img;
-	
+
+	public final static int GRID_SIZE = 20;
+
 	/**
 	 * 
-	 * @param x grid x-coordinate of single-square structure
-	 * @param y grid y-coordinate of single-square structure
-	 * @param img image that defines appearance of the structure
+	 * @param x
+	 *            grid x-coordinate of single-square structure
+	 * @param y
+	 *            grid y-coordinate of single-square structure
+	 * @param img
+	 *            image that defines appearance of the structure
 	 */
 	public Structure(int gridX, int gridY, Image img) {
-		super((2 * gridX + 1) * Grid.SIZE / 2, (2 * gridY + 1) * Grid.SIZE / 2);
+		super((2 * gridX + 1) * Structure.GRID_SIZE / 2, (2 * gridY + 1)
+				* Structure.GRID_SIZE / 2);
 		this.gridX = gridX;
 		this.gridY = gridY;
 	}
-	
+
 	public int getGridX() {
 		return gridX;
 	}
-	
+
 	public int getGridY() {
 		return gridY;
 	}
-
+	
+	public double getDirectionTowards(int x, int y){
+		return Math.tan((y - getY())/ (x - getX()));
+	}
 }
+
