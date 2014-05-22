632c2e2419b0a0dba0392f86ae0db03932f25e24
Vikram Idury <vikramidury@gmail.com>
2014-05-21 22:51:50 -0700

    Add ArcherTower defense

---
+++ src/ArcherTower.java
@@ -0,0 +1,42 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+
+public class ArcherTower extends Defense {
+
+	public static final Image icon = new ImageIcon("archertower.png").getImage();
+	private ArrayList<Enemy> enemies;
+	
+	private ArrayList<Projectile> projectiles;
+
+	public ArcherTower(int x, int y, ArrayList<Enemy> enemies) {
+		super(x, y, ArcherTower.icon);
+		this.enemies = enemies;
+		projectiles = new ArrayList<Projectile>();
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.drawImage(icon, x, y, Structure.GRID_SIZE, Structure.GRID_SIZE, null);
+		
+	}
+
+	@Override
+	public void act() {
+		ArrayList<Enemy> proximity = getEnemiesInProximity(enemies);
+		Enemy target = proximity.get((int)(Math.random() * proximity.size()));
+		double dir = getDirectionTowards(target.getX(), target.getY());
+//		projectiles.add(new Projectile(getX() + Structure.GRID_SIZE/2, getY() + Structure.GRID_SIZE/2, 10, 10, dir,))
+		for (int i = 0; i < projectiles.size(); i++){
+			Projectile p = projectiles.get(i);
+			if (!p.isActive()){ // Change this according to inactive projectile detection
+				projectiles.remove(i);
+			}
+			p.act();
+		}
+	}
+
+}
